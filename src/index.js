e4c2bf5ef0b1659686f350f05c67804563876386
RegistryROK <registry.forbusiness@gmail.com>
2021-05-28 16:01:39 +0900

    init Project Tree

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MenuSelect from "./containers/MenuSelect";
+import WishList from "./containers/WishList";
+import OrderSheet from "./containers/OrderSheet";
+import ErrorPage from "./containers/ErrorPage";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route path="/MenuSelect/:table_no" component={MenuSelect} />
+      <Route path="/OrderSheet/:table_no" component={OrderSheet} />
+      <Route path="/WishList/:table_no" component={WishList} />
+      <Route path="*" component={ErrorPage} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
