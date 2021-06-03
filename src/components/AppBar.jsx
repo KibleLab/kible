b0bae91ae11f58aa755a3a59c1404cd961953820
RegistryROK <registry.forbusiness@gmail.com>
2021-06-03 17:28:31 +0900

    fix: AppBar background white

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,6 +3,7 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
+    background: 'white',
     position: 'fixed',
     width: '100%',
     height: '8%',
