693b6929a48cdb889cbaa4aac8ebf744901da432
RegistryROK <registry.forbusiness@gmail.com>
2021-06-03 16:53:21 +0900

    Apply AppBar & NavBar

---
+++ src/containers/MenuSelect.jsx
@@ -0,0 +1,13 @@
+import AppBar from '../components/AppBar';
+import NavBar from '../components/NavBar';
+
+const MenuSelect = () => {
+  return (
+    <div>
+      <AppBar name={'Menu'} />
+      <NavBar />
+    </div>
+  );
+};
+
+export default MenuSelect;
---
+++ src/containers/WishList.jsx
@@ -0,0 +1,13 @@
+import AppBar from '../components/AppBar';
+import NavBar from '../components/NavBar';
+
+const WishList = () => {
+  return (
+    <div>
+      <AppBar name={'찜목록'} />
+      <NavBar />
+    </div>
+  );
+};
+
+export default WishList;
