56997fa83bd77993844ac47b33a961e2b4b651ab
RegistryROK <registry.forbusiness@gmail.com>
2021-06-03 17:12:50 +0900

    fix: OrderSheet AppBar name

--- src/containers/OrderSheet.jsx
+++ src/containers/OrderSheet.jsx
@@ -1,10 +1,11 @@
 import AppBar from '../components/AppBar';
 import NavBar from '../components/NavBar';
 
-const OrderSheet = () => {
+const OrderSheet = ({match}) => {
+  const table_no = match.params;
   return (
     <div>
-      <AppBar name={'Menu'} />
+      <AppBar name={'주문서 - ' + table_no} />
       <NavBar />
     </div>
   );
