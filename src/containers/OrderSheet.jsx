f051e22f3204cd2beb9d965ca97595f8cd13cf41
RegistryROK <registry.forbusiness@gmail.com>
2021-06-03 17:07:44 +0900

    add: OrderSheet

---
+++ src/containers/OrderSheet.jsx
@@ -0,0 +1,13 @@
+import AppBar from '../components/AppBar';
+import NavBar from '../components/NavBar';
+
+const OrderSheet = () => {
+  return (
+    <div>
+      <AppBar name={'Menu'} />
+      <NavBar />
+    </div>
+  );
+};
+
+export default OrderSheet;
