e52bd3daac5945f27868f1f8909124f504123cee
RegistryROK <registry.forbusiness@gmail.com>
2021-06-04 14:17:54 +0900

    fix: background

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,7 +2,7 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    // background: 'white',
+    background: 'white',
     position: 'fixed',
     width: '100%',
     height: '4rem',
