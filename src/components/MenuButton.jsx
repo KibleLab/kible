80d5140d6763719850f7ff090e041dc5a2e0ae60
RegistryROK <registry.forbusiness@gmail.com>
2021-06-04 17:44:46 +0900

    add: MenuButton

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,43 @@
+import {makeStyles} from '@material-ui/core';
+import {Container, Typography} from '@material-ui/core';
+const useStyles = makeStyles({
+  root: (props) => ({
+    background: '#FFC37C',
+    width: '100%',
+    height: '5rem',
+    padding: '5%',
+    paddingTop: '1.8rem',
+    paddingBottom: '1.8rem',
+    borderRadius: '5px',
+    verticalAlign: 'middle',
+    marginTop: props.top,
+  }),
+  name: {
+    width: '60%',
+    height: '100%',
+    color: 'black',
+    fontSize: '1rem',
+    textAlign: 'left',
+    float: 'left',
+  },
+  price: {
+    width: '40%',
+    height: '100%',
+    color: 'red',
+    fontSize: '1rem',
+    textAlign: 'right',
+    float: 'right',
+  },
+});
+
+const MenuButton = (props) => {
+  const classes = useStyles(props);
+  return (
+    <Container className={classes.root} maxWidth={'false'} onClick={props.onClick}>
+      <Typography className={classes.name}>{props.name}</Typography>
+      <Typography className={classes.price}>{Number(props.price).toLocaleString()}원</Typography>
+    </Container>
+  );
+};
+
+export default MenuButton;
