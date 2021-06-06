6b5727dab0c64966a57d4e5738f70359b80d98c0
RegistryROK <registry.forbusiness@gmail.com>
2021-06-06 16:49:50 +0900

    add: Button

--- src/containers/WishList.jsx
+++ src/containers/WishList.jsx
@@ -1,13 +1,32 @@
 import AppBar from '../components/AppBar';
 import Frame from '../components/Frame';
+import {Button, makeStyles} from '@material-ui/core';
 import NavBar from '../components/NavBar';
 
+const useStyles = makeStyles({
+  button: {
+    background: '#FF006B',
+    width: '100%',
+    height: '2.8rem',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    color: 'white',
+    borderRadius: '10px',
+  },
+});
+
 const WishList = ({match}) => {
+  const classes = useStyles();
   const table_no = match.params.table_no;
   return (
     <div>
       <AppBar name={'찜목록'} />
-      <Frame top={'4rem'} bottom={'8rem'}></Frame>
+      <Frame backColor={'white'} height={'auto'} top={'4rem'} bottom={'8rem'}></Frame>
+      <Frame backColor={'white'} height={'4rem'} bottom={'4rem'} borderTop={'1px solid #a1a1a1'}>
+        <Button className={classes.button} href={'/OrderSheet/' + table_no}>
+          주문서에 추가
+        </Button>
+      </Frame>
       <NavBar value={'wishList'} table_no={table_no} />
     </div>
   );
