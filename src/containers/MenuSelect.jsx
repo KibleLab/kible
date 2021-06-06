102db1d1df77d2a98a254c53fa50213037fc61d6
RegistryROK <registry.forbusiness@gmail.com>
2021-06-07 02:02:31 +0900

    fix: change component Frame to Container/Material-UI & function position

--- src/containers/MenuSelect.jsx
+++ src/containers/MenuSelect.jsx
@@ -1,23 +1,36 @@
+import {makeStyles} from '@material-ui/styles';
+import {Container} from '@material-ui/core';
+
 import AppBar from '../components/AppBar';
-import Frame from '../components/Frame';
-import MenuButton from '../components/MenuButton';
 import NavBar from '../components/NavBar';
 
 const MenuSelect = ({match}) => {
+  const classes = useStyles();
   const table_no = match.params.table_no;
   return (
     <div>
       <AppBar name={'Menu'} />
-      <Frame
-        backColor={'white'}
-        height={'auto'}
-        top={'4rem'}
-        bottom={'4rem'}
-        padding={'0.5rem'}
-      ></Frame>
+      <Container className={classes.body} maxWidth={false}>
+        {/*여기에 map으로 처리된 부분*/}
+      </Container>
       <NavBar value={'menu'} table_no={table_no} />
     </div>
   );
 };
+
+const useStyles = makeStyles({
+  body: {
+    position: 'fixed',
+    background: 'white',
+    width: '100%',
+    height: 'auto',
+    left: 0,
+    top: '4rem',
+    bottom: '4rem',
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem',
+    overflowY: 'auto',
+  },
+});
 
 export default MenuSelect;
