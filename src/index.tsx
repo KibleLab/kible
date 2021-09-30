e15df71ddd1d937ba664ba2405219635273b1b19
RegistryROK <registry.forbusiness@gmail.com>
2021-09-30 21:12:15 +0900

    fix: migration index.jsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MenuSlct from './containers/MenuSlct';
 import WishList from './containers/WishList';
 import OrderSheet from './containers/OrderSheet';
 import ErrorPage from './containers/ErrorPage';
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
@@ -18,13 +17,16 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({ reducer: rootReducer, middleware: [sagaMiddleware] });
 sagaMiddleware.run(rootSaga);
 
+export type RootState = ReturnType<typeof store.getState>;
+export type RootDispatch = typeof store.dispatch;
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
         <Route path='/menu-slct/:table' component={MenuSlct} />
-        <Route path='/ordersheet/:table' component={OrderSheet} />
         <Route path='/wishlist/:table' component={WishList} />
+        <Route path='/ordersheet/:table' component={OrderSheet} />
         <Route path='*' component={ErrorPage} />
       </Switch>
     </Router>
